db007dc9f68a63c969c23bc6d79cd4b55b7cd20f
Sabina Leitoiu <sabina.leitoiu@masabi.com>
2026-01-30 11:11:05 +0000

    Add Constants and RoundUpApiService for network operations

---
+++ app/src/main/java/com/example/roundupapp/data/network/RoundUpApiService.kt
@@ -0,0 +1,25 @@
+package com.example.roundupapp.data.network
+
+import com.example.roundupapp.domain.models.account.Account
+import com.example.roundupapp.utils.Constants.BASE_URL
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Header
+
+private val retrofit = Retrofit.Builder()
+  .addConverterFactory(GsonConverterFactory.create())
+  .baseUrl(BASE_URL)
+  .build()
+
+interface RoundUpApiService {
+
+  @GET("accounts")
+  suspend fun getAccounts(@Header("Authorization") accessToken: String): List<Account>
+}
+
+object RoundUpApi {
+  val retrofitService: RoundUpApiService by lazy {
+    retrofit.create(RoundUpApiService::class.java)
+  }
+}
